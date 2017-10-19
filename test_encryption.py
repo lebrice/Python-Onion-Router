9bb32b19163334334ecf89f720b5885b1f8d7062
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-10-19 15:09:35 -0400

    Added Simple Unit tests for Encryption
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

---
+++ test_encryption.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+from encryption import Encriptor, SimpleAdditionEncriptor
+
+
+class SimpleAdditionEncriptorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.key = 12345
+
+    def test_decrypt_encrypt_equals_original_message(self):
+        message = "Hello there!"
+        encrypted = SimpleAdditionEncriptor.encrypt(message, self.key)
+        decrypted = SimpleAdditionEncriptor.decrypt(encrypted, self.key)
+        self.assertEqual(message, decrypted)
+
+    def test_wrong_key_does_not_decrypt_message(self):
+        message = "Some private message"
+        wrongKey = self.key + 1231  # some other key
+        encrypted = SimpleAdditionEncriptor.encrypt(message, self.key)
+        decrypted = SimpleAdditionEncriptor.decrypt(encrypted, wrongKey)
+        self.assertNotEqual(message, decrypted)
+
+
+
+
