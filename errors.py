61b35dde2331bf06c75be08e246a605ce358128d
Fabrice <fabrice.normandin@gmail.com>
2017-10-30 15:46:32 -0400

    Added Other Error Types

--- errors.py
+++ errors.py
@@ -2,6 +2,14 @@
 """ Module containing our user-defined errors """
 
 
-class OnionSocketError(Exception):
+class OnionError(Exception):
     def __init__(self, *args):
         super().__init__(args)
+
+
+class OnionSocketError(OnionError):
+    pass
+
+
+class OnionNetworkError(OnionError):
+    pass
