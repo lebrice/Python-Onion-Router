bfe689a541a8bf2096558a653a99ea16652dab5b
Fabrice <fabrice.normandin@gmail.com>
2017-10-30 15:18:28 -0400

    Added Basic Test

--- test_onion_socket.py
+++ test_onion_socket.py
@@ -2,8 +2,16 @@
 
 import unittest
 
+from node import OnionNode
+from onion_socket import OnionSocket
+from errors import OnionSocketError
+
 
 class OnionSocketTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
 
-    def test_socket_works(self):
-        print("TODO: Test this out properly.")
+    def test_socket_send_without_connect_raises_error(self):
+        socket = OnionSocket.socket()
+        with self.assertRaises(OnionSocketError):
+            socket.send("something")
