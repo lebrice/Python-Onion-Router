960d1113dd8aa2f64f210780dcd26df21ff2060f
Fabrice <fabrice.normandin@gmail.com>
2017-10-30 15:03:57 -0400

    Adding some pseudocode for creating a message

--- onion_socket.py
+++ onion_socket.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" Module that defines the OnionSocket, used by the user. """
+""" Module that defines the OnionSocket, used by the user to communicate using
+the onion routing network """
+import random
+
+from node import OnionNode
+from errors import OnionSocketError
 
 
 class OnionSocket():
     def __init__(self):
         """ TODO: Create the OnionSocket."""
+        self.my_private_key = 455
+        self.node = OnionNode("my_node", 12350, self.my_private_key)
 
     @staticmethod
-    def socket():
+    def socket() -> OnionSocket:
         """ Creates a new OnionSocket.
 
         (Made to imitate *socket.socket()* method.)
         """
-        raise NotImplementedError()
+        # TODO: Implement this correctly.
+        return OnionSocket()
 
     def connect(self, target_ip_info):
         """ Connects to the given remote host, through the onion network.
         """
-        target_ip, target_port = target_ip_info
         # NOTE: This might be a good place to build/initialize the circuit?
+        self.target_ip, self.target_port = target_ip_info
         raise NotImplementedError()
 
     def send(data: bytes):
@@ -29,3 +37,14 @@ class OnionSocket():
     def recv(self, buffer_size):
         """ Receives the given number of bytes from the Onion socket. """
         raise NotImplementedError()
+
+    def create_message(data):
+        A, B, C = _select_random_neighbours()
+
+    def _select_random_neighbours(self):
+        if not self.node.is_running:
+            raise OnionSocketError("Onion network has not been initialized.")
+        if len(self.node.neighbours) < 3:
+            raise OnionSocketError("""There are currently not enough nodes for
+            an onion network to exist.""")
+        return *random.sample(self.node.neighbours, 3)
