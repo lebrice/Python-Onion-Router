741a51577b18703c9f730ff578122a14df318609
FelixPBelanger <felix.poulinbelanger@hotmail.com>
2017-11-01 19:01:52 +0200

    Added a folder containing an onion router project made in java. (#4)
    * Added Java Onion Routing Project
    
    Signed-off-by: Félix Poulin-Bélanger <felixpoulin-belanger@Felixs-MacBook-Pro-2.local>
    
    * Added a useless comment.
    
    Signed-off-by: Félix Poulin-Bélanger <felixpoulin-belanger@Felixs-MacBook-Pro-2.local>

---
+++ java_onion_router/src/DestinationNode.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+
+// The destination node is temporary.
+public class DestinationNode
+{
+	public static void main(String[] args) throws Exception
+	{	
+		// Port 14444 => Destination Node
+		ServerSocket welcomeSocket = new ServerSocket(14444);
+		
+		while(true)
+		{
+			// Receive from onion node #3.
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromOnionNode3 = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			String receiveMessage = inFromOnionNode3.readLine();
+			printReceivedMessage(connectionSocket.getInetAddress(), connectionSocket.getPort(), receiveMessage);
+			
+			// Convert message to upper case.
+			String sendMessage = receiveMessage.toUpperCase();
+			
+			// Send back to onion node #3.
+			DataOutputStream outToOnionNode3 = new DataOutputStream(connectionSocket.getOutputStream());
+			outToOnionNode3.writeBytes(sendMessage + '\n');
+		}
+	}
+	
+	public static void printReceivedMessage(InetAddress address, int port, String message)
+	{
+		System.out.println("");
+		System.out.println("Destination Node received a message");
+		System.out.println("===================================");
+		System.out.println("Source IP Address:  " + address);
+		System.out.println("Source Port Number: " + port);
+		System.out.println("Content:            " + message);
+	}
+}
