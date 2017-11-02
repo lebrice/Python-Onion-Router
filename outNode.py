49dee02da65ac20e0a6dc057c0cb0f7e0db7fdcd
Maxence Regaudie <mregaudie@gmail.com>
2017-11-02 02:13:52 -0400

    Add files via upload
    Just the Basic structure using Requests library. 
    Can be copy anywhere as longas the library is call.

---
+++ outNode.py
@@ -0,0 +1,24 @@
+import requests
+#LOGIC OF THE END NODE
+
+
+
+
+
+#(Method)Decrypt the message received and return a message
+
+
+msg = "http://api.fixer.io/latest?base=USD"
+
+#(Method)Request the sites and save result 
+#ToDo---------------------------------------------------------------
+
+#Making the request
+r = request.get('')
+
+
+#-------------------------------------------------------------------
+#(Method)Encrypt the answer
+
+
+#(Method)Send back the request base on hashtable
