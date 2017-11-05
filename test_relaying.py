3e99efadbc62d83aab7924817b8c2b1903f4a719
Fabrice <fabrice.normandin@gmail.com>
2017-11-04 21:58:45 -0400

    Added Setup and TearDown methods in test

--- test_relaying.py
+++ test_relaying.py
@@ -4,14 +4,61 @@ import unittest
 import threading
 import socket
 import time
+import json
 
 from relaying import *
 
+HOST = socket.gethostname()
+PORT = 12350
+
 
 class IntermediateRelayTestCase(unittest.TestCase):
-
+    """
+    A --> (B  RELAY  C) --> D
+    """
     def setUp(self):
-        pass
+        """ Set up the sockets as illustrated above. """
+        self.socket_a = socket.socket()
+
+        some_socket = socket.socket()
+        some_socket.bind((HOST, PORT))
+        some_socket.listen()
+
+        def fun_1():
+            time.sleep(0.2)
+            self.socket_a.connect((HOST, PORT))
+
+        some_thread = Thread(target=fun_1)
+        some_thread.start()
+
+        self.socket_b, _ = some_socket.accept()
+        some_thread.join()
+        some_socket.close()
+
+        self.socket_c = socket.socket()
+
+        some_socket = socket.socket()
+        some_socket.bind((HOST, PORT+1))
+        some_socket.listen()
+
+        def fun_2():
+            time.sleep(0.2)
+            self.socket_c.connect((HOST, PORT+1))
+
+        some_thread = Thread(target=fun_2)
+        some_thread.start()
+        self.socket_d, _ = some_socket.accept()
+        some_thread.join()
+        some_socket.close()
+
+    def tearDown(self):
+        global PORT
+        port += 2
+        self.socket_a.close()
+        self.socket_b.close()
+        self.socket_c.close()
+        self.socket_d.close()
+
 
 
     def test_read_from_left_goes_to_right(self):
