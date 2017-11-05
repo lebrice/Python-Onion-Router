4d01fda166bbee5aab707cbbffb25c5b466ce809
Fabrice <fabrice.normandin@gmail.com>
2017-11-04 20:40:39 -0400

    Renamed "previous" to "left" and "next" to "right"

--- relaying.py
+++ relaying.py
@@ -31,14 +31,17 @@ class IntermediateRelay(Thread):
     (analogous to TCP's 'connection' field?)
     """
 
-    def __init__(self, previous_socket: SocketType, next_socket: SocketType):
+    def __init__(self,
+                left_socket: SocketType,
+                right_socket: SocketType,
+                ):
         """ Initializes the relay. """
         super().__init__()
-        self.prev_socket = previous_socket
-        self.next_socket = next_socket
+        self.left_socket = left_socket
+        self.right_socket = right_socket
 
-        self.prev_reader: SocketReader = None
-        self.next_reader: SocketReader = None
+        self.left_reader: SocketReader = None
+        self.right_reader: SocketReader = None
 
     def run(self):
         """ Begin relaying messages from one socket to another.
@@ -53,48 +56,48 @@ class IntermediateRelay(Thread):
 
             until either of them closes its socket.
         """
-        prev_buffer = []  # messages received from the prev_socket.
-        next_buffer = []  # messages received from the next_socket.
+        left_buffer = []  # messages received from the prev_socket.
+        right_buffer = []  # messages received from the next_socket.
 
-        self.prev_reader = SocketReader(self.prev_socket, prev_buffer)
-        self.next_reader = SocketReader(self.next_socket, next_buffer)
+        self.left_reader = SocketReader(self.left_socket, left_buffer)
+        self.right_reader = SocketReader(self.right_socket, right_buffer)
 
-        self.prev_reader.start()
-        self.next_reader.start()
+        self.left_reader.start()
+        self.right_reader.start()
 
-        while (not self.prev_reader.closed) and (not self.next_reader.closed):
+        while (not self.left_reader.closed) and (not self.right_reader.closed):
             # send to next node
-            for message in prev_buffer:
-                self.process_and_send(message, self.next_socket)
-                prev_buffer.remove(message)
+            for message in left_buffer:
+                self.process_and_send(message, self.right_socket)
+                left_buffer.remove(message)
 
             # send to prev node
-            for message in next_buffer:
-                self.process_and_send(message, self.prev_socket)
-                next_buffer.remove(message)
+            for message in right_buffer:
+                self.process_and_send(message, self.left_socket)
+                right_buffer.remove(message)
 
         # One of the two closed.
 
         # IMPORTANT: whenever one of the nodes closes its socket, we assume
         # that whatever message may come after that point can be dumped.
 
-        if self.prev_reader.closed:
+        if self.left_reader.closed:
             # Prev is done sending messages. Send the the rest of its messages
             # over to next, then close next.
-            for message in prev_buffer:
-                self.process_and_send(message, self.next_socket)
-                prev_buffer.remove(message)
-            self.next_socket.shutdown(flag=socket.SHUT_WR)
-            self.next_socket.close()
+            for message in left_buffer:
+                self.process_and_send(message, self.right_socket)
+                left_buffer.remove(message)
+            self.right_socket.shutdown(flag=socket.SHUT_WR)
+            self.right_socket.close()
 
-        elif self.next_reader.closed:
+        elif self.right_reader.closed:
             # Prev is done sending messages. Send the the rest of its messages
             # over to next, then close next.
-            for message in next_buffer:
-                self.process_and_send(message, self.prev_socket)
-                next_buffer.remove(message)
-            self.prev_socket.shutdown(flag=socket.SHUT_WR)
-            self.prev_socket.close()
+            for message in right_buffer:
+                self.process_and_send(message, self.left_socket)
+                right_buffer.remove(message)
+            self.left_socket.shutdown(flag=socket.SHUT_WR)
+            self.left_socket.close()
 
         else:
             raise RuntimeError("Some weird error ocurred.")
