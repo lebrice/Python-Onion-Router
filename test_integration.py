b61d145ab08588561d150bab196c3ae54535f74f
Fabrice <fabrice.normandin@gmail.com>
2017-11-21 13:38:53 -0500

    Removed "network_creator" file, moved logic in integration test

--- test_integration.py
+++ test_integration.py
@@ -2,8 +2,15 @@
 """ Integration testing """
 import unittest
 
+import node
+
+from node import DirectoryNode, OnionNode
+
 
 class IntegrationTestCase(unittest.TestCase):
+    """
+    Integration Tests for the Onion Network.
+    """
     def setUp(self):
         self._setup_network()
         self._start_all()
@@ -30,3 +37,20 @@ class IntegrationTestCase(unittest.TestCase):
         for node in self.onion_nodes:
             node.stop()
         self.directory_node.stop()
+
+
+def generate_nodes(onion_node_count=10, starting_port=12345):
+    """Generates a directory node and some onion nodes.
+
+    Returns: (DirectoryNode, List[OnionNode])
+    """
+    directory_node = DirectoryNode(receiving_port=starting_port)
+    onion_nodes = []
+    for i in range(onion_node_count):
+        node_name = f"ONION_NODE_{i}"
+        node_receiving_port = starting_port + i
+        node_private_key = 127 + i  # Some lousy private key.
+        node = OnionNode(node_name, node_receiving_port, node_private_key)
+        onion_nodes.append(node)
+
+    return directory_node, onion_nodes
